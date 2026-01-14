dcd3d9bb047a4a356e3af6d97913a2ad584d1f20
blandfort <consulting@pblandfort.com>
2026-01-14 18:25:59 +0000

    retry on refusals

--- choices/experiments/simple_rates.py
+++ choices/experiments/simple_rates.py
@@ -430,6 +430,7 @@ async def run_simple_experiment(
         K=1,  # Single response per prompt
         verbose=verbose,
         reasoning_mode=prompt_config.reasoning_mode,
+        valid_choices=["A", "B"],  # Retry responses that don't contain a valid choice
     )
 
     if verbose:
